8811719849f478e140cf61f6214858356430c603
savinshin <savinbelilya@gmail.com>
2025-05-08 15:42:58 +0200

    20-physics

--- src/script.js
+++ src/script.js
@@ -39,21 +39,41 @@ const environmentMapTexture = cubeTextureLoader.load([
 const world = new CANNON.World()
 world.gravity.set(0, -9.82, 0)
 
+// MATERIALS
+const concreteMaterial = new CANNON.Material('concrete')
+const plasticMaterial = new CANNON.Material('plastic')
+
+const concretePlasticContactMaterial = new CANNON.ContactMaterial(
+    concreteMaterial,
+    plasticMaterial,
+    {
+        friction: 0.1,
+        restitution: 0.7
+    }
+)
+
+world.addContactMaterial(concretePlasticContactMaterial)
+
 // SPHERE
 const sphereShape = new CANNON.Sphere()
 const sphereBody = new CANNON.Body({
     mass: 1,
     position: new CANNON.Vec3(0, 3, 0),
-    shape: sphereShape
+    shape: sphereShape,
+    material: plasticMaterial
 })
 world.addBody(sphereBody)
 
 // FLOOR
 const floorShape = new CANNON.Plane()
 const floorBody = new CANNON.Body()
+floorBody.material = concreteMaterial
 // floorBody.mass = 0
 floorBody.addShape(floorShape)
-floorBody.quaternion
+floorBody.quaternion.setFromAxisAngle(
+    new CANNON.Vec3(-1, 0, 0),
+    Math.PI * 0.5
+)
 world.addBody(floorBody)
 
 
